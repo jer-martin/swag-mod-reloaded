e7fac62f5e20e60f4b47bc504195262fbefe6d25
jer-martin <martin.jeremym@gmail.com>
2020-04-23 19:49:34 -0400

    Initial commit

---
+++ src/main/java/com/theboys/swagmod/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package com.theboys.swagmod.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+}
---
+++ src/main/java/com/theboys/swagmod/util/Reference.java
@@ -0,0 +1,12 @@
+package com.theboys.swagmod.util;
+
+public class Reference {
+
+		public static final String MOD_ID = "swagmod";
+		public static final String NAME = "Swag Mod";
+		public static final String VERSION = "Alpha 1.0";
+		public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+		public static final String CLIENT_PROXY_CLASS = "com.theboys.swagmod.proxy.ClientProxy";
+		public static final String COMMON_PROXY_CLASS = "com.theboys.swagmod.proxy.CommonProxy";
+		
+}
