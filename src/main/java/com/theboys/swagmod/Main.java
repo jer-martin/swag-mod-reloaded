49d295b6dfecab72c50bedb2095424121cf1a241
jer-martin <martin.jeremym@gmail.com>
2020-04-23 22:00:38 -0400

    Update Main.java

--- src/main/java/com/theboys/swagmod/Main.java
+++ src/main/java/com/theboys/swagmod/Main.java
@@ -16,26 +16,26 @@ public class Main {
 
 	@Instance
 	public static Main instance;
-	
+
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
 	public static CommonProxy proxy;
-	
+
 	@EventHandler
 	public static void PreInit(FMLPreInitializationEvent event)
 	{
-		
+
 	}
-	
+
 	@EventHandler
 	public static void init(FMLInitializationEvent event)
 	{
-		
+
 	}
-	
+
 	@EventHandler
 	public static void Postinit(FMLPostInitializationEvent event)
 	{
-		
+
 	}
-	
+
 }
